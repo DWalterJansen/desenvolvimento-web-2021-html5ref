f48c7d6b8f8ef65d1d28b545667f1686baee1005
DWalterJansen <40320356+DWalterJansen@users.noreply.github.com>
2021-02-28 12:38:12 -0300

    :fire: Server

--- server/routes/main-routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-const express = require('express')
-const router = express.Router()
-
-router.get('/',  (req, res) => {
-    res.sendFile('index.html');
-})
-
-module.exports = router
